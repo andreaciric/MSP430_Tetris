d2dc631ded5268163b2d802ee24b88d745d8a1f8
Andrea Ciric <andreaciric23@gmail.com>
2021-06-14 18:50:43 +0200

    Add files via upload

---
+++ tetris/Debug/MSP430F55xx_usci_spi_standard_master.d
@@ -0,0 +1,54 @@
+# FIXED
+
+MSP430F55xx_usci_spi_standard_master.obj: ../MSP430F55xx_usci_spi_standard_master.c
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430f5529.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/in430.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h
+MSP430F55xx_usci_spi_standard_master.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h
+
+../MSP430F55xx_usci_spi_standard_master.c:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h:
+
---
+++ tetris/Debug/main.d
@@ -0,0 +1,51 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: /Applications/ti/ccs1030/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h
+main.obj: /Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h
+
+../main.c:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+/Applications/ti/ccs1030/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs1030/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h:
+
